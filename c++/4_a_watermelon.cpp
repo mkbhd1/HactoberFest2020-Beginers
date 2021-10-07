598d306578982039898d56cf935898790f421a86
mkbhd1 <72164071+mkbhd1@users.noreply.github.com>
2021-10-08 01:54:12 +0530

    Add files via upload

---
+++ c++/4_a_watermelon.cpp
@@ -0,0 +1,24 @@
+/*
+  Contest 4
+  Problem A : Watermelon
+  Author: Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 14/01/2020
+ */
+
+#include <cstdio>
+
+int main() {
+    int weight = 0;
+    scanf("%d", &weight);
+
+    if (weight > 2) {
+        if (weight % 2)
+            printf("NO\n");
+        else
+            printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+
+    return 0;
+}
